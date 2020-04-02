0f8b820c77408e1a07bee374e7c7ab691f1bff29
Vojta Janousek <janousek315@gmail.com>
2020-04-02 20:19:13 +0200

    Sanic request types and parameters

---
+++ Sanic/Basics/request_types2.py
@@ -0,0 +1,18 @@
+import sanic
+
+
+app = sanic.Sanic()
+
+
+@app.get('/get')
+async def get_context(request):
+    return sanic.response.text('Hit was with GET')
+
+
+@app.post('/post')
+async def post_context(request):
+    return sanic.response.text('Hit was with POST')
+
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=8080)
