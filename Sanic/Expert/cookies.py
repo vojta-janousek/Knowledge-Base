278079aa1cc22a142a4fb685b468696d1f9cd042
Vojta Janousek <janousek315@gmail.com>
2020-04-03 10:31:41 +0200

    Cookies

---
+++ Sanic/Expert/cookies.py
@@ -0,0 +1,22 @@
+import sanic
+
+
+app = sanic.Sanic()
+
+
+@app.route('/expose')
+async def expose(request):
+    return sanic.response.text(request.cookies.get('testcookie'))
+
+
+@app.route('/set')
+async def set_cookie(request):
+    response = sanic.response.text('It is working')
+    response.cookies['test'] = 'It is the test cookie'
+    response.cookies['test']['domain'] = 'localhost'
+    response.cookies['test']['httponly'] = True
+    return response
+
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=8080)
